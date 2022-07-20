56c101549066b95d11de608ef7d642f1b41e347c
funkaclau <102591258+funkaclau@users.noreply.github.com>
2022-07-20 11:22:41 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,27 @@
+ENTRYPOINT = "https://wax.api.atomicassets.io/atomicassets/v1/transfers"
+FILE_TX = "processed_txs.txt"
+PATH_USERFILES = "./"
+PATH_OTHERFILES = "./"
+RECIPIENT = "pixeltycoons"
+MEMO = "food"
+LIMIT = 5
+CALORIES_AMOUNT = {
+    "250073": 50,
+    "383313": 60,
+    "383213": 60,
+    "349209": 45,
+    "296078": 35,
+    "296075": 120,
+    "296072": 25,
+    "289214": 65,
+    "250071": 30,
+    "249915": 80,
+    "242235": 35,
+    "241874": 1,
+    "240416": 15,
+    "240412": 40,
+    "240404": 60,
+    "240388": 40,
+    "240379": 25,
+    "240374": 100,
+}
